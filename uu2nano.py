0fdcf16fd23f2285418882edfe255ac90f6ba7d0
Aleksandr Karpinskii <homm86@gmail.com>
2023-07-24 13:18:03 +0300

    Consts

--- uu2nano.py
+++ uu2nano.py
@@ -11,30 +11,30 @@ def _make_alpharev(alphabet):
     return bytes(alpharev)
 
 
-alphabet = b'_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-alpharev = _make_alpharev(alphabet)
-_low_mask = 2 ** 62 - 1
-_const_bits = 0b10 << 62
+ALPHABET = b'_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ALPHAREV = _make_alpharev(ALPHABET)
+_LOW_MASK = 2 ** 62 - 1
+_CONST_BITS = 0b10 << 62
 
 
-def uuid_to_nanoid(uu: uuid.UUID, *, alphabet=alphabet) -> str:
+def uuid_to_nanoid(uu: uuid.UUID, *, alphabet=ALPHABET) -> str:
     uu = uu.int
     assert uu >> 62 & 0b11 == 2, "Wrong mark bits. Use fix_uuid() for true random input"
-    uu = (uu >> 64 << 62) | (uu & _low_mask)
+    uu = (uu >> 64 << 62) | (uu & _LOW_MASK)
     b = bytearray(21)
     for i in range(21):
         b[20 - i] = alphabet[uu >> (6 * i) & 0b111111]
     return b.decode()
 
 
-def nanoid_to_uuid(nano: str, *, alpharev=alpharev) -> uuid.UUID:
+def nanoid_to_uuid(nano: str, *, alpharev=ALPHAREV) -> uuid.UUID:
     uu = 0
     for c in nano.encode():
         uu = (uu << 6) | alpharev[c]
-    uu = (uu >> 62 << 64) | (uu & _low_mask) | _const_bits
+    uu = (uu >> 62 << 64) | (uu & _LOW_MASK) | _CONST_BITS
     return uuid.UUID(int=uu)
 
 
 def fix_uuid(uu: uuid.UUID) -> uuid.UUID:
-    uu = uu.int & ~(0b11 << 62) | _const_bits
+    uu = uu.int & ~(0b11 << 62) | _CONST_BITS
     return uuid.UUID(int=uu)
